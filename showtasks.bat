77e015d95663295d9e5cc4ad99f954801303d772
nasz_login_do_github <nasz_email_do_github>
2020-07-06 11:00:54 +0200

    kodilla-23.1

--- showtasks.bat
+++
@@ -1,9 +0,0 @@
-call "runcrud.bat"
-if "%ERRORLEVEL%" == "0" goto openbrauser
-echo.
-echo Can't opened Runcrud
-
-:openbrauser
-start firefox http://localhost:8080/crud/v1/task/getTasks
-echo.
-echo Work is finished.
